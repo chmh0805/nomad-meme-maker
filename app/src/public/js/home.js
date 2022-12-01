a8eec972e025725c6c3a6a65a2e51e1a1fc50e0f
Minhyuk Jung <chmh0805@gmail.com>
2022-12-01 22:42:21 +0900

    Feat: Draw a Person

--- app/src/public/js/home.js
+++ app/src/public/js/home.js
@@ -4,15 +4,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
+ctx.fillRect(210 - 40, 200 - 45, 15, 100);
+ctx.fillRect(350 - 40, 200 - 45, 15, 100);
+ctx.fillRect(260 - 40, 200 - 45, 60, 200);
 
-// ctx.beginPath();
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 90, 7, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 90, 7, Math.PI, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(250, 120, 15, 0, Math.PI);
 ctx.fill();
