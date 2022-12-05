2746d7ef6426ebc260f87f359fcd938c3de0730d
Minhyuk Jung <chmh0805@gmail.com>
2022-12-06 00:08:23 +0900

    Feat: Mouse Painting

--- app/src/public/js/home.js
+++ app/src/public/js/home.js
@@ -4,18 +4,30 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = ["#ff3838", "#ffb8b8", "c56cf0", "#ff9f1a", "7d5fff"];
+ctx.moveTo(200, 200);
 
-function onClick(e) {
-  console.log(e);
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(e.offsetX, e.offsetY);
-  ctx.stroke();
-  ctx.beginPath();
+function onMouseMove(e) {
+  if (isPainting) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(e.offsetX, e.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function startPainting(e) {
+  isPainting = true;
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}
+
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
